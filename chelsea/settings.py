c610e03b2a90910346c082abb5364ced1c9801a3
James-Leigh Taylor <jamesleight@googlemail.com>
2024-01-06 23:41:04 +0000

    Removed Django Secret Key

--- chelsea/settings.py
+++ chelsea/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = "django-insecure-=gy)sp*z+$0^*r)pfwwm-8ma)a*0u4les6af*&zf=2&_bl3m^d"
+
 SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
@@ -40,15 +40,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    #third-party-apps
-    'api',
     'rest_framework',
     'django_extensions', 
     
    
 ]
 
-#AUTH_USER_MODEL = 'accounts.User'
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
